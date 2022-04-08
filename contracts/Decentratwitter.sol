b5cfbe6a6f612d6bdd5b707de05102e37f1d4671
Ethan Cemer <tylercemer@gmail.com>
2022-04-08 14:55:10 -0500

    cleaned up comments

--- contracts/Decentratwitter.sol
+++ contracts/Decentratwitter.sol
@@ -1,10 +1,4 @@
 //SPDX-License-Identifier: MIT
-// Two pages to app: Home and Profile
-// Must have NFT to post content
-// NFT will represent avatar. Store username and image as NFT metadata
-// For profile page, if user has not created an NFT yet then a form will appear that allows users to mint their own NFT profile.
-// For profile page, if user has created an NFT then it will display the users profile picture and username at the top of the page
-// and below that there will be a form at the bottom that allows users to create a new NFT for their profile.
 
 pragma solidity ^0.8.0;
 
